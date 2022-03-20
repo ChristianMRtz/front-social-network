2ca6423a6934fa3702e3baf45525b92c5fc2506d
ChristianMRtz <christianmr268@gmail.com>
2022-03-20 17:28:43 -0500

    Added file Components and Global Styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Index from "./components/Index";
+import GlobalStyles from "./stylesheets/GlobalStyles";
 
 function App() {
   return (
     <>
-    <h1>Holi</h1>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </Router>
     </>
   );
 }
